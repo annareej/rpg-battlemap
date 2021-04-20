cebb59643c2425c50c171a6e3b2201c5b97c7de4
Anna-Reetta Julkunen <ar.julkunen@gmail.com>
2021-04-20 21:28:19 +0300

    Updated TokenTest to use changed constructor.

--- RPGBattlemap/src/test/java/domain/TokenTest.java
+++ RPGBattlemap/src/test/java/domain/TokenTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import static junit.framework.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,39 +11,45 @@ import rpgbattlemap.domain.Token;
 
 public class TokenTest {
     Grid grid;
-    Token token;
+    Token medium;
+    Token large;
+    Token huge;
+    Token gargantuan;
     
     @Before
     public void setUp() {
         grid = new Grid(5,5);
-        token = new Token(grid.getSquare(2, 2), 1); 
+        medium = new Token(grid.getSquare(2, 2), 1, new Circle());
+        large = new Token(grid.getSquare(0, 0), 2, new Circle());
+        huge = new Token(grid.getSquare(0, 0), 3, new Circle());
+        gargantuan = new Token(grid.getSquare(0, 0), 4, new Circle());
     }
     
     @Test
-    public void tokenMovesUp(){
-        token.moveUp(grid);
-        Square square = token.getPosition();
+    public void tokenMovesUp() {
+        medium.moveUp(grid);
+        Square square = medium.getPosition();
         assertEquals(1, square.getY());
     }
     
     @Test
-    public void tokenMovesDown(){
-        token.moveDown(grid);
-        Square square = token.getPosition();
+    public void tokenMovesDown() {
+        medium.moveDown(grid);
+        Square square = medium.getPosition();
         assertEquals(3, square.getY());
     }
     
     @Test
-    public void tokenMovesRight(){
-        token.moveRight(grid);
-        Square square = token.getPosition();
+    public void tokenMovesRight() {
+        medium.moveRight(grid);
+        Square square = medium.getPosition();
         assertEquals(3, square.getX());
     }
     
     @Test
-    public void tokenMovesLeft(){
-        token.moveLeft(grid);
-        Square square = token.getPosition();
+    public void tokenMovesLeft() {
+        medium.moveLeft(grid);
+        Square square = medium.getPosition();
         assertEquals(1, square.getX());
     }
 }
